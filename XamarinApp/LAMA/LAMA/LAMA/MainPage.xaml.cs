8235539a088ac85c116d5676d59b3d90f84dc2ea
Trampod <54006787+Trampod@users.noreply.github.com>
2022-05-02 12:47:23 +0200

    mapsui showcase

--- XamarinApp/LAMA/LAMA/LAMA/MainPage.xaml.cs
+++ XamarinApp/LAMA/LAMA/LAMA/MainPage.xaml.cs
@@ -4,15 +4,35 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace LAMA
 {
     public partial class MainPage : ContentPage
     {
+        private MapHandler mapHandler;
+
         public MainPage()
         {
             InitializeComponent();
+
+            mapHandler = new MapHandler(mapView);
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            while (status != PermissionStatus.Granted)
+            {
+                await DisplayAlert("Error", "Please, have your location turned on and accept the permission.", "OK");
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+
+            mapHandler.CenterOnLocation();
+
         }
     }
 }
