bb8d7cb1187affdd10e4ff39c9baf539f57de9b5
Trampod <54006787+Trampod@users.noreply.github.com>
2022-05-11 22:14:19 +0200

    Added Activity list

---
+++ XamarinApp/LAMA/LAMA/LAMA/ViewModels/ActivityListViewModel.cs
@@ -0,0 +1,67 @@
+﻿using LAMA.Models;
+using LAMA.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LAMA.ViewModels
+{
+    public class ActivityListViewModel
+    {
+
+        public Xamarin.Forms.Command AddActivityCommand { get; }
+        public ObservableCollection<LarpActivity> LarpActivityListItems { get; }
+
+        INavigation Navigation;
+
+        public ActivityListViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+
+            LarpActivityListItems = new ObservableCollection<LarpActivity>();
+
+            //LarpActivity activity = new LarpActivity() { name = "Příprava přepadu", start = new Time(60 * 12 + 45), eventType = LarpActivity.EventType.preparation };
+
+            LarpActivity larpActivity = new LarpActivity(0, "Příprava přepadu", "", "", LarpActivity.EventType.preparation, new EventList<int>(),
+                new Time(60 + 30), 0, new Time(60 * 12 + 45), new Pair<double, double>(0, 0), LarpActivity.Status.launched, new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+            LarpActivityListItems.Add(larpActivity);
+            
+            larpActivity = new LarpActivity(0, "Přepad karavanu", "", "", LarpActivity.EventType.preparation, new EventList<int>(),
+                new Time(60 + 30), 0, new Time(60 * 14 + 15), new Pair<double, double>(0, 0), LarpActivity.Status.launched, new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+            LarpActivityListItems.Add(larpActivity);
+            
+            larpActivity = new LarpActivity(0, "Příprava záchrany", "", "", LarpActivity.EventType.preparation, new EventList<int>(),
+                new Time(60 + 30), 0, new Time(60 * 14), new Pair<double, double>(0, 0), LarpActivity.Status.launched, new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+            LarpActivityListItems.Add(larpActivity);
+            
+            larpActivity = new LarpActivity(0, "Záchrana kupce", "", "", LarpActivity.EventType.preparation, new EventList<int>(),
+                new Time(60 + 30), 0, new Time(60 * 16 + 10), new Pair<double, double>(0, 0), LarpActivity.Status.launched, new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+            LarpActivityListItems.Add(larpActivity);
+
+            larpActivity = new LarpActivity(0, "Úklid mrtvol hráčů", "", "", LarpActivity.EventType.preparation, new EventList<int>(),
+                new Time(60 + 30), 0, new Time(60 * 16 + 15), new Pair<double, double>(0, 0), LarpActivity.Status.launched, new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+            LarpActivityListItems.Add(larpActivity);
+
+            AddActivityCommand = new Xamarin.Forms.Command(OnAddActivityListItem);
+
+            //LarpActivityListItems.Add(new LarpActivity() { name = "Příprava přepadu", start = new Time(60 * 12 + 45), eventType = LarpActivity.EventType.preparation });
+            //LarpActivityListItems.Add(new LarpActivity() { name = "Přepad karavanu", start = new Time(60 * 14 + 15), eventType = LarpActivity.EventType.normal });
+            //LarpActivityListItems.Add(new LarpActivity() { name = "Příprava záchrany", start = new Time(60 * 14), eventType = LarpActivity.EventType.preparation });
+            //LarpActivityListItems.Add(new LarpActivity() { name = "Záchrana kupce", start = new Time(60 * 16 + 10), eventType = LarpActivity.EventType.normal });
+            //LarpActivityListItems.Add(new LarpActivity() { name = "Úklid mrtvol hráčů", start = new Time(60 * 16 + 15), eventType = LarpActivity.EventType.preparation });
+        }
+
+        private async void OnAddActivityListItem(object obj)
+        {
+            //await Shell.Current.GoToAsync(nameof(NewActivityPage));
+
+            await Navigation.PushAsync(new NewActivityPage());
+            
+            //LarpActivity larpActivity = new LarpActivity(0, "New Activity", "", "", LarpActivity.EventType.preparation, new EventList<int>(),
+            //    new Time(0), 0, new Time(0), new Pair<double, double>(0, 0), LarpActivity.Status.launched, new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+            //LarpActivityListItems.Add(larpActivity);
+        }
+    }
+}
