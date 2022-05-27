b34728369dc5ab0b7163c93a30695898421c509e
Trampod <54006787+Trampod@users.noreply.github.com>
2022-05-27 08:15:24 +0200

    preparation for communication initiation

--- XamarinApp/LAMA/LAMA/LAMA/ViewModels/LoginViewModel.cs
+++ XamarinApp/LAMA/LAMA/LAMA/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using LAMA.Views;
+﻿using LAMA.Communicator;
+using LAMA.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,16 +10,33 @@ namespace LAMA.ViewModels
     public class LoginViewModel : BaseViewModel
     {
         public Xamarin.Forms.Command LoginCommand { get; }
+        public Xamarin.Forms.Command FakeLoginCommand { get; }
 
         public LoginViewModel()
         {
             LoginCommand = new Xamarin.Forms.Command(OnLoginClicked);
+            FakeLoginCommand = new Xamarin.Forms.Command(OnFakeLoginClicked);
         }
 
         private async void OnLoginClicked(object obj)
         {
+            string name = "";
+            string IP = "";
+            int port = 0;
+            string password = "";
+            string serverName = "";
+            string clientName = "";
+
+            new ServerCommunicator(name, IP, port, password);
+            new ClientCommunicator(serverName, password, clientName);
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(MapPage)}");
+        }
+
+        private async void OnFakeLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(MapPage)}");
         }
     }
 }
