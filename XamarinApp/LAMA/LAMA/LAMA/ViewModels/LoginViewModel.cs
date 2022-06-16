8133ecf12b3f89373c2aab482c86b69319860978
Trampod <54006787+Trampod@users.noreply.github.com>
2022-06-16 09:47:25 +0200

    New page for testing pages without connecting to database

--- XamarinApp/LAMA/LAMA/LAMA/ViewModels/LoginViewModel.cs
+++ XamarinApp/LAMA/LAMA/LAMA/ViewModels/LoginViewModel.cs
@@ -98,7 +98,7 @@ namespace LAMA.ViewModels
         private async void OnFakeLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(MapPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(TestPage)}");
         }
     }
 }
---
+++ XamarinApp/LAMA/LAMA/LAMA/Views/TestPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using LAMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LAMA.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPage : ContentPage
+    {
+        LarpActivity activity;
+
+        public TestPage()
+        {
+            InitializeComponent();
+
+            activity = new LarpActivity(4, "Testování Xamarin Aplikace",
+                "Je potřeba otestovat zda aplikace funguje.\nProjít každou funkcionalitu a zda tam nejsou chyby.",
+                "Udělat aplikaci.\nZaplnit ji daty.\nVyrazit do přírody.",
+                LarpActivity.EventType.normal, new EventList<int>(),
+                new Time(60 + 30), 0, new Time(60 * 16 + 15), new Pair<double, double>(0, 0), LarpActivity.Status.launched,
+                new EventList<Pair<int, int>>(), new EventList<Pair<string, int>>(), new EventList<Pair<int, string>>());
+
+        }
+
+        async void OnDisplayActivity(object sender, EventArgs args)
+        {
+            //await Shell.Current.GoToAsync($"//{nameof(DisplayActivityPage)}");
+            await Navigation.PushAsync(new DisplayActivityPage(activity));
+        }
+    }
+}
