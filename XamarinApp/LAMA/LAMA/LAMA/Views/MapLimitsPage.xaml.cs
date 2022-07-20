8288c68afd0364f88c70b325df1ee37c1375e724
Ermith <f.s.ermith@gmail.com>
2022-07-20 14:03:15 +0200

    Finally, it works. The bug is resolved. The location is saved.

--- XamarinApp/LAMA/LAMA/LAMA/Views/MapLimitsPage.xaml.cs
+++ XamarinApp/LAMA/LAMA/LAMA/Views/MapLimitsPage.xaml.cs
@@ -2,26 +2,67 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using LAMA.Services;
+using System.Threading.Tasks;
+using Mapsui;
+using Mapsui.UI.Forms;
+using Xamarin.Essentials;
 
 namespace LAMA.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapLimitsPage : ContentPage
     {
+        private MapView mapView;
         public MapLimitsPage()
         {
             InitializeComponent();
+        }
 
-            double minExt = Math.Min(mapView.Viewport.Extent.MinX, mapView.Viewport.Extent.MinY);
-            double maxExt = Math.Max(mapView.Viewport.Extent.MaxX, mapView.Viewport.Extent.MaxY);
-            MapHandler.SetPanLimits(mapView, minExt, minExt, maxExt, maxExt);
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var layout = Content as StackLayout;
+
+            var indicator = new ActivityIndicator();
+            indicator.IsRunning = true;
+            indicator.IsVisible = true;
+            layout.Children.Add(indicator);
+
+            await Task.Delay(1000);
+            mapView = new MapView
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.Fill,
+                BackgroundColor = Color.Gray,
+                HeightRequest = 500
+            };
+
+            MapHandler.Instance.MapViewSetup(mapView);
+            MapHandler.Instance.SetLocationVisible(mapView, false);
+
+            layout.Children.Add(mapView);
+            layout.Children.Remove(indicator);
+            indicator.IsRunning = false;
         }
 
         async void OnSaveLimits(object sender, EventArgs e)
         {
-            MapHandler.globalZoomLimit = mapView.Viewport.Resolution;
-            MapHandler.globalPanLimt = mapView.Viewport.Extent;
-            await Navigation.PopAsync();
+            Mapsui.Geometries.Point p = Mapsui.Projection.SphericalMercator.ToLonLat(mapView.Viewport.Center.X, mapView.Viewport.Center.Y);
+            Location loc = new Location
+            {
+                Latitude = p.Y,
+                Longitude = p.X
+            };
+            MapHandler.Instance.CurrentLocation = loc;
+            (Content as StackLayout).Children.Remove(mapView);
+            mapView = null;
+            await Navigation.PushAsync(new MapPage());
+        }
+
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
         }
     }
 }
