a1e3548edea4358d5352a2d23d7bc302105eb393
Trampod <54006787+Trampod@users.noreply.github.com>
2022-09-27 18:00:14 +0200

    wip selection page

--- XamarinApp/LAMA/LAMA/LAMA/ViewModels/ActivitySelectionViewModel.cs
+++ XamarinApp/LAMA/LAMA/LAMA/ViewModels/ActivitySelectionViewModel.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using LAMA.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace LAMA.ViewModels
 {
     public class ActivitySelectionViewModel
     {
+        public LarpActivity Activity;
+
+        public ObservableCollection<ActivityListItemViewModel> LarpActivityListItems { get; }
+
+        private ActivityListItemViewModel _selectedItem;
+        public ActivityListItemViewModel SelectedItem { get { return _selectedItem; } set
+            {
+                if (value == null)
+                    return;
+                _selectedItem = value;
+                Activity = _selectedItem.LarpActivity;
+                Close();
+            }
+        }
+
+
+        public ActivitySelectionViewModel()
+		{
+            Activity = null;
+            LarpActivityListItems = new ObservableCollection<ActivityListItemViewModel>();
+
+            for (int i = 0; i < DatabaseHolder<LarpActivity, LarpActivityStorage>.Instance.rememberedList.Count; i++)
+            {
+                LarpActivityListItems.Add(new ActivityListItemViewModel(DatabaseHolder<LarpActivity, LarpActivityStorage>.Instance.rememberedList[i]));
+            }
+		}
+
+        private async void Close()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
     }
 }
