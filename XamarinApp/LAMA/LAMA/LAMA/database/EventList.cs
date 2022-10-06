b144a238f8ebc040ed34bf06539b62d1d104b94a
Trampod <54006787+Trampod@users.noreply.github.com>
2022-10-06 11:31:22 +0200

    rebase fix

--- XamarinApp/LAMA/LAMA/LAMA/database/EventList.cs
+++ XamarinApp/LAMA/LAMA/LAMA/database/EventList.cs
@@ -54,13 +54,5 @@ namespace LAMA
                 this.Add(item);
             }
         }
-        
-        public override string ToString()
-        {
-            if(Count == 0)
-                return string.Empty;
-            else 
-                return base.ToString();
-        }
     }
 }
