b71739c281efcdd10402f768a72a8e9bc19c3f21
Jan-Koblizek <48485433+Jan-Koblizek@users.noreply.github.com>
2022-10-13 10:18:51 +0200

    Chat Channels

---
+++ XamarinApp/LAMA/LAMA/LAMA/ViewModels/ChatChannelsViewModel.cs
@@ -0,0 +1,91 @@
+﻿using BruTile;
+using LAMA.Models;
+using LAMA.Singletons;
+using LAMA.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LAMA.ViewModels
+{
+    public class ChatChannelsViewModel : INotifyPropertyChanged
+    {
+        public Xamarin.Forms.Command ChannelCreatedCommand { get; }
+        public Command<object> ChatChannelTapped { get; private set; }
+        public string ChannelName { get; set; }
+        public ObservableCollection<String> Channels { get; set; }
+
+        INavigation Navigation;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void OnChannelCreated()
+        {
+            if (ChannelName != "")
+            {
+                LarpEvent.ChatChannels.Add(ChannelName);
+            }
+            Debug.WriteLine($"ChatCannels: {LarpEvent.ChatChannels.Count}");
+            ChannelName = "";
+            OnPropertyChanged("ChannelName");
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ChatChannelsViewModel(INavigation navigation)
+        {
+            LarpEvent.ChatChannels.dataChanged += PropagateChanged;
+            ChannelCreatedCommand = new Xamarin.Forms.Command(OnChannelCreated);
+            ChatChannelTapped = new Command<object>(DisplayChannel);
+
+            Navigation = navigation;
+
+            Channels = new ObservableCollection<string>();
+
+            for (int i = 0; i < LarpEvent.ChatChannels.Count; i++)
+            {
+                Channels.Add(LarpEvent.ChatChannels[i]);
+            }
+        }
+
+        private void PropagateChanged()
+        {
+            Debug.WriteLine("PropagateChanged");
+            Channels.Clear();
+
+            for (int i = 0; i < LarpEvent.ChatChannels.Count; i++)
+            {
+                Channels.Add(LarpEvent.ChatChannels[i]);
+            }
+
+            Debug.WriteLine(Channels.Count);
+        }
+
+        private async void DisplayChannel(object obj)
+        {
+            Debug.WriteLine("DisplayChannel");
+            if (obj.GetType() != typeof(string))
+            {
+                await App.Current.MainPage.DisplayAlert("Message", "Object is of wrong type.\nExpected: " + typeof(string).Name
+                    + "\nActual: " + obj.GetType().Name, "OK");
+                return;
+            }
+
+            string channelName = (string)obj;
+            Debug.WriteLine(channelName);
+            await Navigation.PushAsync(new ChatTest(channelName));
+        }
+    }
+}
--- XamarinApp/LAMA/LAMA/LAMA/Views/ChatChannels.xaml.cs
+++ XamarinApp/LAMA/LAMA/LAMA/Views/ChatChannels.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LAMA.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,13 @@ using Xamarin.Forms.Xaml;
 namespace LAMA.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class ChatChannels : ContentPage
     {
-        public Page1()
+        public ChatChannels()
         {
             InitializeComponent();
+
+            BindingContext = new ChatChannelsViewModel(Navigation);
         }
     }
 }
