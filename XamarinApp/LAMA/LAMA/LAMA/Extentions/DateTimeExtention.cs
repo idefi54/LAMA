1c2c7684f9c00d06f3cfcc2b8fa8f8ecf4adbfeb
Trampod <54006787+Trampod@users.noreply.github.com>
2022-11-03 01:36:56 +0100

    Helper functions for converting between DateTime and UnixTimeStamp

---
+++ XamarinApp/LAMA/LAMA/LAMA/Extentions/DateTimeExtention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAMA.Extentions
+{
+	public static class DateTimeExtention
+	{
+		public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+		{
+			DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+			dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+			return dateTime;
+		}
+
+		public static long ToUnixTimeSeconds(this DateTime dateTime)
+		{
+			DateTime foo = DateTime.Now;
+			long unixTime = ((DateTimeOffset)foo).ToUnixTimeSeconds();
+			return unixTime;
+		}
+	}
+}
