450888cce79ad01a4efa7d7021d530125c144bfa
Ermith <f.s.ermith@gmail.com>
2022-12-15 10:11:36 +0100

    Location tracking service.

---
+++ XamarinApp/LAMA/LAMA/LAMA/Services/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAMA.Services
+{
+    public class StartServiceMessage
+    {
+    }
+
+    public class StopServiceMessage
+    {
+    }
+
+    public class LocationMessage
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class LocationErrorMessage
+    {
+
+    }
+}
