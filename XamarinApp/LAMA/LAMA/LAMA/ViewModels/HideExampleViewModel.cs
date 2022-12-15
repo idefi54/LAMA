8ecf0cd3a5b05bf866dd28d55a97ea7d64ced32d
Trampod <54006787+Trampod@users.noreply.github.com>
2022-12-15 12:00:13 +0100

    Added hidable example for other elements

--- XamarinApp/LAMA/LAMA/LAMA/ViewModels/HideExampleViewModel.cs
+++ XamarinApp/LAMA/LAMA/LAMA/ViewModels/HideExampleViewModel.cs
@@ -9,7 +9,7 @@ namespace LAMA.ViewModels
 	{
 
 		public Command TestHideCommand { get; }
-		private bool _testHideBool = false;
+		private bool _testHideBool = true;
 		public bool TestHideBool { get { return _testHideBool; } set { SetProperty(ref _testHideBool, value); } }
 
 		private void OnTestHide(object obj)
