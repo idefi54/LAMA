fb47adbdf1316603a9536ed8531ba086b78a50cb
idefixCZ <59373818+idefi54@users.noreply.github.com>
2023-03-01 19:11:21 +0100

    done and editable

---
+++ XamarinApp/LAMA/LAMA/LAMA/Views/POIDetailsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using LAMA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LAMA.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class POIDetailsView : ContentPage
+    {
+        public POIDetailsView()
+        {
+            InitializeComponent();
+            BindingContext = new POIViewModel(Navigation, null);
+        }
+        public POIDetailsView(PointOfInterest poi)
+        {
+            InitializeComponent();
+            BindingContext = new POIViewModel(Navigation, poi);
+        }
+
+    }
+}
