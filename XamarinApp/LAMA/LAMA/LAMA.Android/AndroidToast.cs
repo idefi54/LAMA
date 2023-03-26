d295119a45cdd862cc6c28b2a4cfa14890520651
idefixCZ <59373818+idefi54@users.noreply.github.com>
2023-03-26 17:25:07 +0200

    toasty

---
+++ XamarinApp/LAMA/LAMA/LAMA.Android/AndroidToast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+[assembly: Xamarin.Forms.Dependency(typeof(LAMA.Droid.ToastAndroid))]
+namespace LAMA.Droid
+{
+    public class ToastAndroid : ToastInterface
+    {
+        public void DoTheThing(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+        }
+    }
+}
---
+++ XamarinApp/LAMA/LAMA/LAMA.iOS/IOSToast.cs
@@ -0,0 +1,59 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(LAMA.iOS.IOSToast))]
+namespace LAMA.iOS
+{
+    
+    public class IOSToast:ToastInterface
+    {
+        public void DoTheThing(string message)
+        {
+            ShowAlert(message, 3.0);
+        }
+
+
+
+        // borrowed from https://stackoverflow.com/questions/35279403/toast-equivalent-for-xamarin-forms
+        const double LONG_DELAY = 3.5;
+        const double SHORT_DELAY = 2.0;
+
+        NSTimer alertDelay;
+        UIAlertController alert;
+
+        public void LongAlert(string message)
+        {
+            ShowAlert(message, LONG_DELAY);
+        }
+        public void ShortAlert(string message)
+        {
+            ShowAlert(message, SHORT_DELAY);
+        }
+
+        void ShowAlert(string message, double seconds)
+        {
+            alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) =>
+            {
+                dismissMessage();
+            });
+            alert = UIAlertController.Create(null, message, UIAlertControllerStyle.Alert);
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+        }
+
+        void dismissMessage()
+        {
+            if (alert != null)
+            {
+                alert.DismissViewController(true, null);
+            }
+            if (alertDelay != null)
+            {
+                alertDelay.Dispose();
+            }
+        }
+    }
+}
